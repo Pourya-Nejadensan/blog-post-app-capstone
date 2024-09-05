935614f32274029f32be046594b11bb9dcf1e785
Pourya <pourya.nejadensan@gmail.com>
2024-09-05 16:10:18 +0200

    add style to Footer

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,7 +1,25 @@
+import styled from 'styled-components';
+import { Box, Typography } from '@mui/material';
+
 export default function Footer() {
     return (
-        <footer>
-            <p>&copy; 2024 My Project. All rights reserved.</p>
-        </footer>
+        <FooterContainer>
+            <Typography variant="body2">
+                &copy; 2024 My Project. All rights reserved.
+            </Typography>
+        </FooterContainer>
     );
 }
+
+
+const FooterContainer = styled(Box)`
+    left: 0;
+    right: 0;
+    width: 100%;
+    background-color: #1976d2;
+    color: white;
+    padding: 20px;
+    position: fixed;
+    bottom: 0;
+    text-align: center;
+`;
