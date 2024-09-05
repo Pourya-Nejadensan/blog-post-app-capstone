9f3448146ee12cff5f22e23e48cd99876422db04
Pourya <pourya.nejadensan@gmail.com>
2024-09-05 11:37:07 +0200

    make the website responsive and centralize

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 import AppRoutes from './components/common/Routes';
+import { Container, Box, Stack } from '@mui/material';
 
 function App() {
     return (
-        <div>
-            <AppRoutes />
-        </div>
+        <Container>
+            <Box display="flex" justifyContent="center">
+                <Stack spacing={2} sx={{ width: '100%', maxWidth: { xs: '100%', sm: '90%', md: '80%' } }}>
+                    <AppRoutes />
+                </Stack>
+            </Box>
+        </Container>
     );
 }
 
