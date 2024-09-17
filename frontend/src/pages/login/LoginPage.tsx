4a9de5f4e0f22f7cb5fd596167248e944497aa1b
Pourya <pourya.nejadensan@gmail.com>
2024-09-17 17:53:14 +0200

    impel a simple github login function

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,7 +1,13 @@
 export default function LoginPage(){
+    function handleLogin() {
+        const host: string = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin;
+
+        window.open(host + "/oauth2/authorization/github", "_self");
+            }
     return (
         <div>
             <h1>Login Page</h1>
+            <button onClick={handleLogin}>Login</button>
         </div>
     );
 }
