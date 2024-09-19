46aef97f3f4ef35c0026715b85ce9e757292719f
Pourya <pourya.nejadensan@gmail.com>
2024-09-19 11:42:39 +0200

    add style to Contact.tsx

--- frontend/src/components/common/Contact.tsx
+++ frontend/src/components/common/Contact.tsx
@@ -1,8 +1,32 @@
+import styled from "styled-components";
+
 export default function Contact() {
     return (
-        <div>
-            <h2>Contact Us</h2>
-            <p>If you have any questions, feel free to reach out to us at pourya.nejadensan@gmail.com.</p>
-        </div>
+        <StyledContainer>
+            <StyledHeading>Contact Us</StyledHeading>
+            <StyledParagraph>If you have any questions, feel free to reach out to us at pourya.nejadensan@gmail.com.</StyledParagraph>
+        </StyledContainer>
     );
 }
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+`;
+
+const StyledHeading = styled.h2`
+    font-size: 2.8em;
+    color: #1a237e;
+    margin-bottom: 30px;
+`;
+
+const StyledParagraph = styled.p`
+    font-size: 1.4em;
+    color: #424242;
+    text-align: center;
+    max-width: 750px;
+`;
