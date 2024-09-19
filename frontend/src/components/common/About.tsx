5c34e3d551e3d06329b79c390e36b9c4dc5a44d8
Pourya <pourya.nejadensan@gmail.com>
2024-09-19 11:41:48 +0200

    add style to About.tsx

--- frontend/src/components/common/About.tsx
+++ frontend/src/components/common/About.tsx
@@ -1,8 +1,32 @@
+import styled from "styled-components";
+
 export default function About() {
     return (
-        <div>
-            <h2>About Us</h2>
-            <p>Welcome to our blog! We share the latest news and articles on various topics.</p>
-        </div>
+        <StyledContainer>
+            <StyledHeading>About Us</StyledHeading>
+            <StyledParagraph>Welcome to our blog! We share the latest news and articles on various topics.</StyledParagraph>
+        </StyledContainer>
     );
 }
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+`;
+
+const StyledHeading = styled.h2`
+    font-size: 2.8em;
+    color: #1a237e;
+    margin-bottom: 30px;
+`;
+
+const StyledParagraph = styled.p`
+    font-size: 1.4em;
+    color: #424242;
+    text-align: center;
+    max-width: 750px;
+`;
