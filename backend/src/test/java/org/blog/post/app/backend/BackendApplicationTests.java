9dd3f58b4d1a80944b587d2ca4e2dc583ffb6c48
Pourya <pourya.nejadensan@gmail.com>
2024-09-20 11:29:51 +0200

    add some test for BackendApplicationTests to better code coverage

--- backend/src/test/java/org/blog/post/app/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/blog/post/app/backend/BackendApplicationTests.java
@@ -1,13 +1,23 @@
 package org.blog.post.app.backend;
 
+import org.blog.post.app.backend.post.controller.PostController;
+import org.blog.post.app.backend.post.service.impl.PostServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext.getBean(PostController.class), "PostController bean should be loaded");
+        assertNotNull(applicationContext.getBean(PostServiceImpl.class), "PostServiceImpl bean should be loaded");
     }
-
 }
